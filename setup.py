acf34fe6081eacfb0aa13277bef31efc1982b821
Kukosk <kukosk@gmail.com>
2020-09-01 20:51:38 +0200

    bumping up the version to 1.0.4

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ CHANGELOG = open(os.path.join(os.path.dirname(__file__), 'CHANGELOG.rst')).read(
 
 setup(
     name='pyapns_client',
-    version='1.0.3',
+    version='1.0.4',
     packages=['pyapns_client'],
     include_package_data=True,
     license='MIT License',
