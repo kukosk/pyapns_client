889225bb64c03e65a145811357f35893db38e538
Kukosk <jakub@klen.sk>
2022-06-10 00:09:04 +0200

    bumping up the version to 2.0.6

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ CHANGELOG = open(os.path.join(os.path.dirname(__file__), 'CHANGELOG.rst')).read(
 
 setup(
     name='pyapns_client',
-    version='2.0.5',
+    version='2.0.6',
     packages=['pyapns_client'],
     include_package_data=True,
     license='MIT License',
