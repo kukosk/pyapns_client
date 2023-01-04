0a6f4b4d8836ef3734afb21030da058125c096e1
Aleksandr Soloshenko <capcom2me@gmail.com>
2023-01-04 09:14:12 +0700

    Added: auto versioning

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ CHANGELOG = open(os.path.join(os.path.dirname(__file__), "CHANGELOG.rst")).read(
 
 setup(
     name="pyapns_client3",
-    version="3.0.2",
+    version="3.0.3",
     packages=["pyapns_client"],
     include_package_data=True,
     license="MIT License",
